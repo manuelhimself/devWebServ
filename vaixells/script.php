8299358bee7e3b48bb501bd88f95b40a541fde39
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-06 13:23:11 +0100

    Update script.php

--- vaixells/script.php
+++ vaixells/script.php
@@ -6,56 +6,6 @@
     <title>Vaixells i Propietaris</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <?php
-
-    include 'ships.php';
-    include 'owner.php';
-
-    $vaix1 = new ships(33, "veler", "1234UUU", 2002);
-    $vaix2 = new ships(3, "lanxa", "3456KKK", 2002);
-    $vaix3 = new ships(42, "llaut", "5678PRS", 2002);
-    $vaix4 = new ships(500, "creuer", "9012KAS", 2002);
-
-    $prop1 = new owner("Pep", "PER", $vaix1);
-    $prop2 = new owner("Joan", "PNB", $vaix2);
-    $prop3 = new owner("Pere", "PdY", $vaix3);
-    $prop4 = new owner("Toni", "CdY", $vaix4);
-
-    $vaixArr = array($vaix1, $vaix2, $vaix3, $vaix4);
-    $propArr = array($prop1, $prop2, $prop3, $prop4);
-
-    function showShips()
-    {
-        global $vaixArr;
-
-        for ($i = 0; $i < count($vaixArr); $i++) {
-            echo "<li>" . $vaixArr[$i] . "</li>";
-        }
-    }
-
-    function showOwners()
-    {
-        global $propArr;
-
-        for ($i = 0; $i < count($propArr); $i++) {
-            echo "<li>" . $propArr[$i] . "</li>";
-        }
-    }
-
-    function introOwnerBoat()
-    {
-        global $propArr;
-        global $vaixArr;
-        if (isset($_GET["eslora"], $_GET["tipus"], $_GET["matricula"], $_GET["any"])) {
-            $vaix = new ships($_GET["eslora"], $_GET["tipus"], $_GET["matricula"], $_GET["any"]);
-            $prop = new owner($_GET["nom"], $_GET["llicencia"], $vaix);
-            array_push($propArr, $prop);
-            array_push($vaixArr, $vaix);
-        }
-    }
-
-    ?>
-
 </head>
 
 <body>
@@ -67,6 +17,53 @@
 
     <ul>
         <?php
+
+        include 'ships.php';
+        include 'owner.php';
+
+        $vaix1 = new ships(33, "veler", "1234UUU", 2002);
+        $vaix2 = new ships(3, "lanxa", "3456KKK", 2002);
+        $vaix3 = new ships(42, "llaut", "5678PRS", 2002);
+        $vaix4 = new ships(500, "creuer", "9012KAS", 2002);
+
+        $prop1 = new owner("Pep", "PER", $vaix1);
+        $prop2 = new owner("Joan", "PNB", $vaix2);
+        $prop3 = new owner("Pere", "PdY", $vaix3);
+        $prop4 = new owner("Toni", "CdY", $vaix4);
+
+        $vaixArr = array($vaix1, $vaix2, $vaix3, $vaix4);
+        $propArr = array($prop1, $prop2, $prop3, $prop4);
+
+        function showShips()
+        {
+            global $vaixArr;
+
+            for ($i = 0; $i < count($vaixArr); $i++) {
+                echo "<li>" . $vaixArr[$i] . "</li>";
+            }
+        }
+
+        function showOwners()
+        {
+            global $propArr;
+
+            for ($i = 0; $i < count($propArr); $i++) {
+                echo "<li>" . $propArr[$i] . "</li>";
+            }
+        }
+
+        function introOwnerBoat()
+        {
+            global $propArr;
+            global $vaixArr;
+            if (isset($_GET["eslora"], $_GET["tipus"], $_GET["matricula"], $_GET["any"])) {
+                $vaix = new ships($_GET["eslora"], $_GET["tipus"], $_GET["matricula"], $_GET["any"]);
+                $prop = new owner($_GET["nom"], $_GET["llicencia"], $vaix);
+                array_push($propArr, $prop);
+                array_push($vaixArr, $vaix);
+            }
+        }
+
         if (isset($_GET["option"])) {
             if ($_GET["option"] == 1) {
                 showShips();
